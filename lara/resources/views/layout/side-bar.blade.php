1a651f353bf55ef553c655fcea057dd64daeb400
Md Rana Hossain <rana.hossain@sslwireless.com>
2020-07-04 02:16:42 +0600

    side bar modify and templating update

---
+++ lara/resources/views/layout/side-bar.blade.php
@@ -0,0 +1,54 @@
+<!-- Side menu START -->
+<div id="sidebar" class="sidebar-offcanvas">
+    <ul class="nav flex-column nav-sidebar">
+        <li class="nav-item active">
+            <a class="nav-link active" href="javascript:void(0)">
+                <svg class="icon-sprite">
+                    <use xlink:href="assets/images/icons-sprite.svg#home"/>
+                </svg>
+                Home
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="javascript:void(0)">
+                <svg class="icon-sprite">
+                    <use xlink:href="assets/images/icons-sprite.svg#bulb-eco"/>
+                </svg>
+                Lights
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="javascript:void(0)">
+                <svg class="icon-sprite">
+                    <use xlink:href="assets/images/icons-sprite.svg#camera"/>
+                </svg>
+                Cameras
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="javascript:void(0)">
+                <svg class="icon-sprite">
+                    <use xlink:href="assets/images/icons-sprite.svg#appliances"/>
+                </svg>
+                Appliances
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="javascript:void(0)">
+                <svg class="icon-sprite">
+                    <use xlink:href="assets/images/icons-sprite.svg#thermometer"/>
+                </svg>
+                <span>Climate</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="javascript:void(0)">
+                <svg class="icon-sprite">
+                    <use xlink:href="assets/images/icons-sprite.svg#settings"/>
+                </svg>
+                Settings
+            </a>
+        </li>
+    </ul>
+</div>
+<!-- Side menu END -->
